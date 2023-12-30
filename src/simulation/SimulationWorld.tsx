277762639884850452fd1b4d8710393e1b2ba0cf
leon_schwarzenberger <leon.schwarzenberger@aclue.de>
2023-12-30 12:53:24 +0100

    add Basecomponents

--- src/simulation/SimulationWorld.tsx
+++ src/simulation/SimulationWorld.tsx
@@ -1,5 +1,6 @@
 import {Canvas} from "@react-three/fiber";
 import Controls from "./Controls";
+import {ThreeBox, ThreeLine} from "./component/BaseComponents";
 
 
 function SimulationWorld() {
@@ -8,11 +9,10 @@ function SimulationWorld() {
         <Canvas style={{width: '100%', height: '100%'}}>
             <Controls/>
             <ambientLight />
-            <pointLight position={[100, 100, 100]} />
-            <mesh>
-                <boxGeometry args={[1, 1, 1]} />
-                <meshStandardMaterial color="orange" />
-            </mesh>
+            <pointLight position={[10, 10, 10]} />
+            <ThreeBox position={[0,0,0]} rotation={[0,0,0]} size={[20,0.1,20]} color={'gray'} />
+            <ThreeBox position={[1,1,1]} rotation={[0,0,0]} size={[2,1,0.5]} color={'green'} />
+            <ThreeLine points={[[2,0.05,2],[5,0.05,5]]} color={'red'} />
         </Canvas>
     );
 }
---
+++ src/simulation/component/BaseComponents.tsx
@@ -0,0 +1,48 @@
+import {Box, Cylinder, Sphere} from "@react-three/drei/core/shapes";
+import { Line } from '@react-three/drei'
+import * as THREE from "three";
+
+export type ThreeArr3 = [number, number, number];
+
+export interface BaseComponentProps {
+    position: ThreeArr3,
+    rotation: ThreeArr3,
+    size: ThreeArr3,
+    color: string,
+}
+export interface LineProps {
+    points: ThreeArr3[],
+    width?: number,
+    color: string,
+}
+
+export function ThreeBox({position, rotation, size, color}: BaseComponentProps) {
+    const material = new THREE.MeshLambertMaterial({color, });
+
+    return (
+        <Box position={position} rotation={rotation} args={size} material={material}/>
+    );
+}
+
+export function ThreeCylinder({position, rotation, size, color}: BaseComponentProps) {
+    const material = new THREE.MeshLambertMaterial({color});
+
+    return (
+        <Cylinder position={position} rotation={rotation} args={size} material={material}/>
+    );
+}
+
+export function ThreeSphere({position, rotation, size, color}: BaseComponentProps) {
+    const material = new THREE.MeshLambertMaterial({color});
+
+    return (
+        <Sphere position={position} rotation={rotation} args={[size[0]]} material={material}/>
+    );
+}
+
+export function ThreeLine({points, width, color}: LineProps) {
+
+    return (
+        <Line points={points} color={color} lineWidth={width ?? 5}/>
+    );
+}
